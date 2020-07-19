9a586d21d9398c05a36aa2b4ed2c8e1a56fd80b2
Kyle Domingo <kdomingo@wayfair.com>
2020-07-19 10:54:48 +0200

    laravel service provider

--- src/DI/OauthServiceProvider.php
+++ src/DI/OauthServiceProvider.php
@@ -3,20 +3,41 @@
 namespace KOA2\DI;
 
 use Illuminate\Support\ServiceProvider;
-use KOA2\Persistence\AccessToken;
-use KOA2\Persistence\AccessTokenInterface;
-use KOA2\Persistence\Client;
-use KOA2\Persistence\ClientInterface;
+use KOA2\PDO\ConnectionConfigurationProvider;
+use KOA2\PDO\ConnectionConfigurationProviderInterface;
+use KOA2\PDO\PDOConnection;
+use KOA2\PDO\PDOConnectionInterface;
+use KOA2\Persistence\Contract\AccessTokenPersistence as AccessTokenPersistenceInterface;
+use KOA2\Persistence\Contract\AuthCodePersistence as AuthCodePersistenceInterface;
+use KOA2\Persistence\Contract\ClientPersistence as ClientPersistenceInterface;
+use KOA2\Persistence\Contract\RefreshTokenPersistence as RefreshTokenPersistenceInterface;
+use KOA2\Persistence\Contract\ScopePersistence as ScopePersistenceInterface;
+use KOA2\Persistence\Contract\UserPersistence as UserPersistenceInterface;
+use KOA2\Persistence\DB\AccessToken as AccessTokenPersistence;
+use KOA2\Persistence\DB\AuthCode as AuthCodePersistence;
+use KOA2\Persistence\DB\Client as ClientPersistence;
+use KOA2\Persistence\DB\RefreshToken as RefreshTokenPersistence;
+use KOA2\Persistence\DB\Scope as ScopePersistence;
+use KOA2\Persistence\DB\User as UserPersistence;
+use KOA2\Repository\AccessTokenRepository;
+use KOA2\Repository\AuthCodeRepository;
 use KOA2\Repository\ClientRepository;
-use KOA2\Repository\ClientRepositoryInterface;
-use KOA2\Service\ClientAuthenticator;
-use KOA2\Service\ClientAuthenticatorInterface;
-use KOA2\Service\ClientAuthorizer;
-use KOA2\Service\ClientAuthorizerInterface;
-use KOA2\Service\EncryptionKeyProvider;
-use KOA2\Service\EncryptionKeyProviderInterface;
-use KOA2\Service\TokenProvider;
-use KOA2\Service\TokenProviderInterface;
+use KOA2\Repository\Contract\AccessTokenRepositoryInterface;
+use KOA2\Repository\Contract\AuthCodeRepositoryInterface;
+use KOA2\Repository\Contract\RefreshTokenRepositoryInterface;
+use KOA2\Repository\Contract\ScopeRepositoryInterface;
+use KOA2\Repository\Contract\UserRepositoryInterface;
+use KOA2\Repository\RefreshTokenRepository;
+use KOA2\Repository\ScopeRepository;
+use KOA2\Repository\UserRepository;
+use KOA2\Service\AccessTokenProvider;
+use KOA2\Service\Authorizer;
+use KOA2\Service\BcryptPasswordHasher;
+use KOA2\Service\Contract\AccessTokenProviderInterface;
+use KOA2\Service\Contract\AuthorizerInterface;
+use KOA2\Service\Contract\OauthServerProviderInterface;
+use KOA2\Service\Contract\PasswordHasherInterface;
+use KOA2\Service\OauthServerProvider;
 
 class OauthServiceProvider extends ServiceProvider
 {
@@ -27,15 +48,32 @@ class OauthServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(EncryptionKeyProviderInterface::class, EncryptionKeyProvider::class);
+        // DB
+        $this->app->bind(ConnectionConfigurationProviderInterface::class, ConnectionConfigurationProvider::class);
+        $this->app->bind(PDOConnectionInterface::class, PDOConnection::class);
+        // Hasher
+        $this->app->bind(PasswordHasherInterface::class, BcryptPasswordHasher::class);
 
-        $this->app->bind(ClientAuthorizerInterface::class, ClientAuthorizer::class);
-        $this->app->bind(ClientAuthenticatorInterface::class, ClientAuthenticator::class);
-        $this->app->bind(ClientRepositoryInterface::class, ClientRepository::class);
-        $this->app->bind(ClientInterface::class, Client::class);
+        // Repository
+        $this->app->bind(AccessTokenRepositoryInterface::class, AccessTokenRepository::class);
+        $this->app->bind(AuthCodeRepositoryInterface::class, AuthCodeRepository::class);
+        $this->app->bind(\KOA2\Repository\Contract\ClientRepositoryInterface::class, ClientRepository::class);
+        $this->app->bind(RefreshTokenRepositoryInterface::class, RefreshTokenRepository::class);
+        $this->app->bind(ScopeRepositoryInterface::class, ScopeRepository::class);
+        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
 
-        $this->app->bind(TokenProviderInterface::class, TokenProvider::class);
-        $this->app->bind(AccessTokenInterface::class, AccessToken::class);
+        // Persistence
+        $this->app->bind(AccessTokenPersistenceInterface::class, AccessTokenPersistence::class);
+        $this->app->bind(AuthCodePersistenceInterface::class, AuthCodePersistence::class);
+        $this->app->bind(ClientPersistenceInterface::class, ClientPersistence::class);
+        $this->app->bind(RefreshTokenPersistenceInterface::class, RefreshTokenPersistence::class);
+        $this->app->bind(ScopePersistenceInterface::class, ScopePersistence::class);
+        $this->app->bind(UserPersistenceInterface::class, UserPersistence::class);
+
+        // Services
+        $this->app->bind(OauthServerProviderInterface::class, OauthServerProvider::class);
+        $this->app->bind(AccessTokenProviderInterface::class, AccessTokenProvider::class);
+        $this->app->bind(AuthorizerInterface::class, Authorizer::class);
     }
 
     /**
